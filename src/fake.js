caba0d5947cdef922b4f43a4735c43c067d5c418
ilyaizr <ilyaizrailyan@gmail.com>
2021-05-11 13:44:12 +0300

    BUILD - bugfixes for multiple, updates for slider

--- src/fake.js
+++ src/fake.js
@@ -1,12 +1,31 @@
 export const formConfig = {
 
   fields: {
+    bio: {
+      label: 'Tell us about yourself'
+    },
+    name: {
+      label: 'Ваше имя',
+      value: 'Anton',
+      tab: 2
+    },
+    city: {
+      label: 'City',
+      rules: [a => a && a.length > 3 || 'err msg'],
+      tab: 3,
+    },
+  },
 
-    date: {
-      type: 'date',
-      range: 1,
-      required: 1
-    }
+  tabs: true,  // Thats enough
+
+  // More verbose config
+  tabs: {
+    "header-nav": true,
+    steps: [
+      { title: 'First', icon: 'settings' }, //Icon names: https://material.io/resources/icons/
+      { title: 'Second', icon: 'img:https://cdn.quasar.dev/logo/svg/quasar-logo.svg' }
+    ]
+    // More properties: https://quasar.dev/vue-components/stepper#qstepper-api
   },
 
   methods: {
