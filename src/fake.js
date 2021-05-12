9a89b2dce2bdff440794e528152d086d9993c52a
ilyaizr <ilyaizrailyan@gmail.com>
2021-05-12 19:15:25 +0300

    BUILD -- Multiple internal logic fix

--- src/fake.js
+++ src/fake.js
@@ -1,33 +1,7 @@
 export const formConfig = {
 
   fields: {
-    simpleMultiple: {
-      type: 'multiple',
-      label: "Multiple with simpleinputs",
-      value: [
-        { firstName: 'Peter', lastName: '' },
-        { firstName: 'Lois', lastName: 'Puttershmidt' }
-      ],
-      settings: {
-        firstName: {
-          label: 'First name',
-        },
-        lastName: {
-          label: 'Last name'
-        },
-      },
-    },
-    react: {
-      onFocus(fb){
-        // console.log({...fb.fields.simpleMultiple.settings});
-        fb.fields.simpleMultiple.settings.lastName.hint = 'new hint'
-        
-        // fb.fields.simpleMultiple.settings.lastName = {
-        //   label: 'Family name',
-        //   hint: 'a.k.a second name'
-        // }
-      }
-    }
+    p: {}
   },
 
 
