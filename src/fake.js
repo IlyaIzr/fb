75d4b589c5b7a1168d482d0dfabc741261007d37
ilyaizr <ilyaizrailyan@gmail.com>
2021-06-21 16:23:15 +0300

    BUILD - multiple index on events, attachments event fixes

--- src/fake.js
+++ src/fake.js
@@ -2,35 +2,6 @@ export const formConfig = {
 
 
   fields: {
-    simpleMultiple: {
-      type: 'multiple',
-      label: "Multiple with simpleinputs",
-      value: [
-        { firstName: 'Peter', lastName: '' },
-        { firstName: 'Lois', lastName: 'Puttershmidt' }
-      ],
-      settings: {
-        firstName: {
-          label: 'First name',
-          onInput(a,b,val, som){
-    
-            console.log('%c⧭', 'color: #f200e2',b, val, som);
-          }
-        },
-        lastName: {
-          label: 'Last name'
-        },
-      },
-    },
-
-    
-    firstName1: {
-      label: 'First name',
-      onInput(a,b,val, som){
-
-        console.log('%c⧭', 'color: #f200e2',b, val, som);
-      }
-    },
   },
 
 
