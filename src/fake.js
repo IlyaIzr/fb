999dfca0a117ff62fd1ec43b2d1a6fa646f72aaf
ilyaizr <ilyaizrailyan@gmail.com>
2021-10-01 15:57:45 +0300

    BUILD -- multiple multivalues fix

--- src/fake.js
+++ src/fake.js
@@ -1,49 +1,13 @@
 export const formConfig = {
   fields: {
-    simpleMultiple: {
-      type: 'multiple',
-      label: "Multiple with simpleinputs",
-      value: [
-        { firstName: 'Lois1', lastName: 'Puttershmidt' },
-        { firstName: 'Lois2', lastName: 'Puttershmidt' },
-        { firstName: 'Lois3', lastName: 'Puttershmidt' },
-        { firstName: 'Lois4', lastName: 'Puttershmidt' }
-      ],
-      settings: {
-        firstName: {
-          label: 'First name', row: 1
-        },
-        lastName: {
-          label: 'Last name', row: 1
-        },
-      },
-    },
   },
 
 
   methods: {
     onSubmit(fb, formComp, values) {
       console.log(values);
-    },
-    async onReset(formGlobal, component) {
-      // Callback functions availible for all event handlers
-      return async (formGlobal) => {
-        // do something AFTER reset
-        formGlobal.title = 'Warning, form has been reseted'
-      }
-    },
+    }
   },
-  buttons: {
-    reset: {label: 'res'}
-  }
 }
 
-export const values = {
-  simpleMultiple: [
-    { firstName: 'Lois1', lastName: 'Puttershmidt' },
-    { firstName: 'Lois2', lastName: 'Puttershmidt' },
-    { firstName: 'Lois3', lastName: 'Puttershmidt' },
-    { firstName: 'Lois4', lastName: 'Puttershmidt' }
-
-  ],
-}
+export const values = {}
